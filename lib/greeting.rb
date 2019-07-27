28fc6a80e0eebb28455575ebf524d94a57e1ae2a
Divya Beech <dyadav1006@gmail.com>
2019-07-27 18:41:30 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,8 +1,6 @@
 # code the #greeting method here!
 
-  puts "Hi, I'm Divya, what's your #{name}" ?
-  def greeting(name)
+  # puts "Hi, I'm Divya, what's your #{name}" ?
+def greeting(name)
     puts "Hello #{name}. It's nice to meet you"
 end
-input = gets.chomp
-greeting(input)
